6f67ef5e5a1cb22541db4ffca054b4e3ce6d8830
bjmontillon <pangilinanbj@gmail.com>
2021-06-04 09:54:45 +0800

    updated @media

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+
     var panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
@@ -11,4 +12,23 @@ for (i = 0; i < acc.length; i++) {
       panel.style.cssText = "display: block";
     }
   });
+
+  
 }
+
+
+
+$(document).ready(function(){
+    $('.accordion').click(function() {
+        $(this).toggleClass('open');
+
+        $accordion_content = $(this).next('.panel');
+
+        $('.panel').not($accordion_content).slideUp();
+        $('.panel').not($accordion_content).prev('.accordion').removeClass('open');
+        $accordion_content.stop(true, true).slideToggle(400)
+
+        // Remove this handle
+        //$('.accordion-title i').toggleClass('rotate');
+    });
+});
